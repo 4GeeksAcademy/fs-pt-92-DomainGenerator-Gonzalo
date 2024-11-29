1cd78955e26d78879ad3e325831c823273f88903
Gonzalo Blanch Dominguez <gonzaloblanchdev@gmail.com>
2024-11-29 10:44:53 +0000

    Primera versión del ejercicio con domain hacks

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let articles = ["the", "a", "our", "my"];
+let adjetives = ["awesome", "terrible", "great", "big"];
+let nouns = ["racoon", "cat", "web", "portfolio", "market", "puedes", "malcom"];
+let domains = ["com", "es", "org", "net"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//let domainComponents = [articles, adjetives, nouns, domains];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+generateDomains();
+
+function generateDomains() {
+  for (let article of articles) {
+    for (let adjetive of adjetives) {
+      for (let noun of nouns) {
+        let domainName = article + adjetive + noun;
+        printDomains(domainName);
+      }
+    }
+  }
+}
+
+function printDomains(domainName) {
+  for (let domain of domains) {
+    if (domainName.endsWith(domain)) {
+      console.log(
+        "DOMAIN HACK DETECTED: " + formatDomainHack(domainName, domain)
+      );
+    } else {
+      console.log(domainName + "." + domain);
+    }
+  }
+}
+
+function formatDomainHack(domainName, domain) {
+  //let domainSubstrIndex = domainName.indexOf(domain);
+  //let newDomainName = domainName.slice(0, domainSubstrIndex);
+  return domainName.slice(0, domainName.indexOf(domain)) + "." + domain;
+}
+
+function generateRandomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
